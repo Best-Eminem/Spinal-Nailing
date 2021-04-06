37d6786d3ec2b793a04f5a16ef3c83777e6b3518
Best-Eminem <cyk_cd@163.com>
2021-04-06 20:52:44 +0800

    change

--- Vertebra-Landmark-Detection-3d/draw_3dgaussian.py
+++ Vertebra-Landmark-Detection-3d/draw_3dgaussian.py
@@ -3,26 +3,6 @@ import matplotlib.pyplot as plt
 import math
 import mpl_toolkits.mplot3d
 import matplotlib as mpl
-
-# x, y = np.mgrid[-2:2:200j, -2:2:200j]
-# z=(1/2*math.pi*3**2)*np.exp(-(x**2+y**2)/2*3**2)
-# ax = plt.subplot(111, projection='3d')
-# ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap='rainbow', alpha=0.9)#绘面
-#
-# ax.set_xlabel('x')
-# ax.set_ylabel('y')
-# ax.set_zlabel('z')
-# plt.show()
-
-# x = np.mgrid[-2:2:200j]
-# y = (1/2*math.pi*3**2)*np.exp(-(x**2)/2*3**2)
-# ax = plt.subplot(11, projection='2d')
-# ax.plot_surface(x, y, rstride=1, cstride=1, cmap='rainbow', alpha=0.9)#绘面
-#
-# ax.set_xlabel('x')
-# ax.set_ylabel('y')
-# #ax.set_zlabel('z')
-# plt.show()
 def gaussian_radius(det_size, min_overlap=0.7):
     height, width = det_size
 
